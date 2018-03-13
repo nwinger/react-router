40cd442fb8b0126cc035484a62840b9ce16c92f6
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-03-13 16:16:08 +0100

    :rocket: :fire:

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Navbar from './Navbar';
+import Players from './Players';
+import Teams from './Teams';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/players" component={Players} />
+            <Route path="/teams" component={Teams} />
+            <Route
+              render={() => <h1 className="text-center">Four oh Four</h1>}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import TeamLogo from './TeamLogo';
+import { getTeamNames } from '../api';
+
+export default class Home extends Component {
+  state = {
+    teamNames: []
+  };
+
+  componentDidMount() {
+    getTeamNames().then(teamNames =>
+      this.setState(() => ({
+        teamNames
+      }))
+    );
+  }
+
+  render() {
+    const { teamNames } = this.state;
+    return (
+      <div className="container">
+        <h1 className="large-header">Hash History Basketball League</h1>
+        <h3 className="header text-center">Select a team</h3>
+        <div className="home-grid">
+          {teamNames.map(id => (
+            <Link key={id} to={`/${id}`}>
+              <TeamLogo id={id} width="125px" />
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
